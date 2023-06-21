d50f412cb4c71ee0f4487ecd950ed747c1d72445
Anton Solodkov <solodkovantosha@gmail.com>
2023-06-22 02:40:01 +0300

    feat: add my-select component, fix: application structure

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,18 +1,30 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import MySelect from "../MySelect/MySelect";
 
 const Header = (props) => {
   const navigate = useNavigate();
-  
+
   const [query, setQuery] = useState("");
   const [categories, setCategories] = useState("");
   const [orderBy, setOrderBy] = useState("relevance");
-
 
   const search = () => {
     props.get(query, categories, orderBy);
 
     navigate("/");
+  };
+
+  const changeQuery = (query) => {
+    setQuery(query.target.value);
+  };
+
+  const changeCategory = (category) => {
+    setCategories(category.target.value);
+  };
+
+  const changeOrderBy = (order) => {
+    setOrderBy(order.target.value);
   };
 
   return (
@@ -21,31 +33,37 @@ const Header = (props) => {
       <div className="search">
         <input
           type="search"
-          placeholder="search"
+          placeholder="search..."
           value={query}
-          onChange={(e) => setQuery(e.target.value)}
+          onChange={changeQuery}
         ></input>
         <button onClick={search}>Search</button>
       </div>
       <div className="filters">
         <div className="filter">
           <p>Categories</p>
-          <select onChange={(e) => setCategories(e.target.value)}>
-            <option value="">All</option>
-            <option value="art">Art</option>
-            <option value="biography">Biography</option>
-            <option value="computers">Computers</option>
-            <option value="history">History</option>
-            <option value="medical">Medical</option>
-            <option value="poetry">Poetry</option>
-          </select>
+          <MySelect
+            options={[
+              { value: "", name: "All" },
+              { value: "art", name: "Art" },
+              { value: "biography", name: "Biography" },
+              { value: "computers", name: "Computers" },
+              { value: "history", name: "History" },
+              { value: "medical", name: "Medical" },
+              { value: "poetry", name: "Poetry" },
+            ]}
+            onChange={changeCategory}
+          />
         </div>
         <div className="filter">
           <p>Sorting by</p>
-          <select onChange={(e) => setOrderBy(e.target.value)}>
-            <option value="relevance">Relevance</option>
-            <option value="newest">Newest</option>
-          </select>
+          <MySelect
+            options={[
+              { value: "relevance", name: "Relevance" },
+              { value: "newest", name: "Newest" },
+            ]}
+            onChange={changeOrderBy}
+          />
         </div>
       </div>
     </header>
---
+++ src/components/MySelect/MySelect.jsx
@@ -0,0 +1,13 @@
+const MySelect = ({ options, onChange }) => {
+  return (
+    <select onChange={onChange}>
+      {options.map((option) => (
+        <option key={option.value} value={option.value}>
+          {option.name}
+        </option>
+      ))}
+    </select>
+  );
+};
+
+export default MySelect;
